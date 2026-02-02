3ded1661cc30c00b2ca11537f468d361c3297d52
KnyazLV <r.belovs@inbox.lv>
2026-02-02 19:16:17 +0200

    add indents

---
+++ languages/oberon/indents.scm
@@ -0,0 +1,25 @@
+(
+  [
+    (module_body)
+    (procedure_body)
+    (if_statement)
+    (while_statement)
+    (repeat_statement)
+    (for_statement)
+    (case_statement)
+    (case_clause)
+    (record_type)
+    (const_decls)
+    (type_decls)
+    (variable_decls)
+  ] @indent
+)
+
+(
+  [
+    (kEnd)
+    (kUntil)
+    (kElse)
+    (kElseif)
+  ] @outdent
+)
