108c6180a2b385031c3ad7308edc61a7032ebb10
KnyazLV <r.belovs@inbox.lv>
2026-02-03 11:04:49 +0200

    feat: add outline support

---
+++ languages/oberon/outline.scm
@@ -0,0 +1,14 @@
+(module
+  (module_header
+    modulename: (ident) @name)
+) @item
+
+(const_decl) @name @item
+
+(type_decl) @name @item
+
+(variable_decl) @name @item
+
+(procedure_decl
+  (procedure_heading) @name
+) @item
